55013fd363a54d6888432169507543b5aaee654c
Carlos Espino Maraví <sigsda32@minedu.gob.pe>
2021-10-05 18:46:45 -0500

    Input, output, event emitter y servicios.

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,25 +1,31 @@
 import { Component } from '@angular/core';
 import { Personaje } from '../interfaces/dbz.interface';
 
+import { DbzService } from '../services/dbz.services';
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html'
 })
 export class MainPageComponent {
 
-  personajes: Personaje[] = [
-    {
-      nombre: 'Goku',
-      poder: 15000
-    },
-    {
-      nombre: 'Vegeta',
-      poder: 7500
-    }
-  ];
+  // personajes: Personaje[] = [];
 
   nuevo: Personaje = {
     nombre: 'Maestro Roshi',
     poder: 1000
   };
+
+  // get personajes() : Personaje[]{
+  //   return this.dbzService.personajes;
+  // }
+
+  // agregarNuevoPersonaje(argumento: Personaje){
+  //   console.log(argumento);
+  //   this.personajes.push(argumento);
+  // }
+
+  constructor(private dbzService: DbzService) {
+    // this.personajes = this.dbzService.personajes;
+  }
 }
